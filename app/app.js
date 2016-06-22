bc4b3700cd7b44ebd1092deea4f8da2fd3f69a6a
Stanislav Alekseevich Chernyavskij <Stanislav Alekseevich Chernyavskij>
2016-06-22 22:04:47 +0300

    og

--- app/app.js
+++ app/app.js
@@ -47,48 +47,98 @@ var app = angular.module('myApp', [
 					templateUrl: 'views/home.html',
 					meta: {
 						title: 'Сити Жалюзи: Производство и продажа жалюзи, штор, ворот.',
-						description: 'Все, что вы бы хотели спросить о жалюзи: где купить жалюзи в Москве на 18% дешевле?'
+						description: 'Все, что вы бы хотели спросить о жалюзи: где купить жалюзи в Москве на 18% дешевле?',
+						ogUrl: 'https://city-jaluzi.ru',
+						ogTitle: 'Сити Жалюзи: Производство и продажа жалюзи, штор, ворот.',
+						ogDescription: 'Все, что вы бы хотели спросить о жалюзи: где купить жалюзи в Москве на 18% дешевле?',
+						ogImage: 'https://city-jaluzi.ru/img/fb.jpg',
 					}
 				})
 				.when('/article/:slug', {
 					controller: 'ArticleCtrl',
-					templateUrl: 'views/article.html'
+					templateUrl: 'views/article.html',
+					meta: {
+						ogUrl: 'https://city-jaluzi.ru',
+						ogTitle: 'Сити Жалюзи: Производство и продажа жалюзи, штор, ворот.',
+						ogDescription: 'Все, что вы бы хотели спросить о жалюзи: где купить жалюзи в Москве на 18% дешевле?',
+						ogImage: 'https://city-jaluzi.ru/img/fb.jpg'
+					}
 				})
 				.when('/search/:term', {
 					controller: 'CatalogCtrl',
-					templateUrl: 'views/catalog.html'
+					templateUrl: 'views/catalog.html',
+					meta: {
+						ogUrl: 'https://city-jaluzi.ru',
+						ogTitle: 'Сити Жалюзи: Производство и продажа жалюзи, штор, ворот.',
+						ogDescription: 'Все, что вы бы хотели спросить о жалюзи: где купить жалюзи в Москве на 18% дешевле?',
+						ogImage: 'https://city-jaluzi.ru/img/fb.jpg'
+					}
 				})
 				.when('/catalog', {
 					controller: 'CatalogCtrl',
 					templateUrl: 'views/catalog.html',
-					reloadOnSearch: false
+					reloadOnSearch: false,
+					meta: {
+						ogUrl: 'https://city-jaluzi.ru',
+						ogTitle: 'Сити Жалюзи: Производство и продажа жалюзи, штор, ворот.',
+						ogDescription: 'Все, что вы бы хотели спросить о жалюзи: где купить жалюзи в Москве на 18% дешевле?',
+						ogImage: 'https://city-jaluzi.ru/img/fb.jpg'
+					}
 				})
 				.when('/catalog/:category', {
 					controller: 'CatalogCtrl',
 					templateUrl: 'views/catalog.html',
-					reloadOnSearch: false
+					reloadOnSearch: false,
+					meta: {
+						ogUrl: 'https://city-jaluzi.ru',
+						ogTitle: 'Сити Жалюзи: Производство и продажа жалюзи, штор, ворот.',
+						ogDescription: 'Все, что вы бы хотели спросить о жалюзи: где купить жалюзи в Москве на 18% дешевле?',
+						ogImage: 'https://city-jaluzi.ru/img/fb.jpg'
+					}
 				})
 				.when('/catalog/:category/:subcategory', {
 					controller: 'CatalogCtrl',
 					templateUrl: 'views/catalog.html',
-					reloadOnSearch: false
+					reloadOnSearch: false,
+					meta: {
+						ogUrl: 'https://city-jaluzi.ru',
+						ogTitle: 'Сити Жалюзи: Производство и продажа жалюзи, штор, ворот.',
+						ogDescription: 'Все, что вы бы хотели спросить о жалюзи: где купить жалюзи в Москве на 18% дешевле?',
+						ogImage: 'https://city-jaluzi.ru/img/fb.jpg'
+					}
 				})
 				.when('/product/:category/:subcategory/:product', {
 					controller: 'ProductPageCtrl',
 					templateUrl: 'views/product.html',
-					reloadOnSearch: false
+					reloadOnSearch: false,
+					meta: {
+						ogUrl: 'https://city-jaluzi.ru',
+						ogTitle: 'Сити Жалюзи: Производство и продажа жалюзи, штор, ворот.',
+						ogDescription: 'Все, что вы бы хотели спросить о жалюзи: где купить жалюзи в Москве на 18% дешевле?',
+						ogImage: 'https://city-jaluzi.ru/img/fb.jpg'
+					}
 				})
 				.when('/product/:category/:subcategory/:product/:texture', {
 					controller: 'ProductPageCtrl',
 					templateUrl: 'views/product.html',
-					reloadOnSearch: false
+					reloadOnSearch: false,
+					meta: {
+						ogUrl: 'https://city-jaluzi.ru',
+						ogTitle: 'Сити Жалюзи: Производство и продажа жалюзи, штор, ворот.',
+						ogDescription: 'Все, что вы бы хотели спросить о жалюзи: где купить жалюзи в Москве на 18% дешевле?',
+						ogImage: 'https://city-jaluzi.ru/img/fb.jpg'
+					}
 				})
 				.when('/constructor', {
 					controller: 'ConstructorPageCtrl',
 					templateUrl: 'views/constructor.html',
 					meta: {
 						title: 'Конструктор жалюзи и штор онлайн. Сити Жалюзи.',
-						description: 'При помощи нашего конструктора вы можете легко подобрать шторы или жалюзи не выходя из дома.'
+						description: 'При помощи нашего конструктора вы можете легко подобрать шторы или жалюзи не выходя из дома.',
+						ogUrl: 'https://design-okna.ru/#!/constructor',
+						ogTitle: 'Онлайн конструктор жалюзи и штор',
+						ogDescription: 'Приложите жалюзи и шторы к вашему окну в реальном времени, выберите цвет и материал.',
+						ogImage: 'https://design-okna.ru/img/fbcon.jpg'
 					}
 				})
 				.otherwise({
